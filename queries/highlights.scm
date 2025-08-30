d3982aac9f839d0ee61f2f298e59f525ac06578b
Meek Msaki <meek10x@gmail.com>
2025-08-30 02:46:20 -0500

    update query names

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,174 +1,155 @@
 ;; ==========================
 ;; Huff Tree-sitter Highlights
+;; Matches naming scheme from source.huff
 ;; ==========================
 
 ;; --------------------------
 ;; Natspec / Documentation
 ;; --------------------------
-(natspec_line) @comment.documentation
-(natspec_block) @comment.documentation
+(natspec_line) @comment.block.documentation.huff
+(natspec_block) @comment.block.documentation.huff
 
-(natspec_tag_title) @keyword
-(natspec_tag_author) @keyword
-(natspec_tag_notice) @keyword
-(natspec_tag_dev) @keyword
-(natspec_tag_param) @keyword
-(natspec_tag_return) @keyword
+(natspec_tag_title) @storage.type.title.natspec
+(natspec_tag_author) @storage.type.author.natspec
+(natspec_tag_notice) @storage.type.notice.natspec
+(natspec_tag_dev) @storage.type.dev.natspec
+(natspec_tag_param) @storage.type.param.natspec
+(natspec_tag_return) @storage.type.return.natspec
 
-(comment_line) @comment
-(comment_block) @comment
-
-;; --------------------------
-;; Declarations
-;; --------------------------
-(declaration_macro name: (identifier) @function.macro)
-(declaration_fn name: (identifier) @function)
-(declaration_jumptable name: (identifier) @constant)
-(declaration_jumptable_packed name: (identifier) @constant)
-
-;; Keywords for declarations
-"#define macro" @keyword
-"#define fn" @keyword  
-"#define jumptable" @keyword
-"#define jumptable__packed" @keyword
-"#define function" @keyword
-"#define event" @keyword
-"#define error" @keyword
-"#define constant" @keyword
-"#define table" @keyword
-"#define test" @keyword
-
-"takes" @keyword
-"returns" @keyword
-"=" @operator
-
-;; Function modifiers
-"view" @keyword
-"pure" @keyword
-"payable" @keyword
-"nonpayable" @keyword
-
+(comment_line) @comment.line.huff
+(comment_block) @comment.block.huff
 
 ;; --------------------------
-;; Interface
+;; Declarations - Storage Keywords
 ;; --------------------------
-(interface_function name: (identifier) @function)
-(interface_event name: (identifier) @function)
-(interface_primitives) @type
-(interface_extensions) @keyword
+"#define macro" @storage.macro.huff
+"#define fn" @storage.macro.huff
+"#define jumptable" @storage.macro.huff
+"#define jumptable__packed" @storage.macro.huff
+"#define function" @storage.function.huff
+"#define event" @storage.event.huff
+"#define error" @storage.function.huff
+"#define constant" @storage.constant.huff
+"#define table" @storage.macro.huff
+"#define test" @storage.macro.huff
 
-;; Parameter modifiers (handled as identifiers now)
-
-;; --------------------------
-;; Error Definitions
-;; --------------------------
-(error_definition name: (identifier) @error)
-
-;; --------------------------
-;; Control / Imports
-;; --------------------------
-(control_import) @keyword
+"takes" @storage.takes.huff
+"returns" @storage.returns.huff
 
 ;; --------------------------
-;; Numbers
+;; Function/Macro Names
 ;; --------------------------
-(number_decimal) @number
-(number_hex) @number
+(declaration_macro name: (identifier) @entity.name.function.huff)
+(declaration_fn name: (identifier) @entity.name.function.huff)
+(declaration_jumptable name: (identifier) @entity.name.function.huff)
+(declaration_jumptable_packed name: (identifier) @entity.name.function.huff)
+(declaration_table name: (identifier) @entity.name.function.huff)
+(declaration_test name: (identifier) @entity.name.function.huff)
+(interface_function name: (identifier) @entity.name.function.huff)
+(interface_event name: (identifier) @entity.name.function.huff)
+(error_definition name: (identifier) @entity.name.function.huff)
 
 ;; --------------------------
-;; Opcodes
+;; Interface Types and Extensions
 ;; --------------------------
-(opcode) @function.builtin
+(interface_primitives) @variable.parameter
+"view" @storage.type.interface.huff
+"pure" @storage.type.interface.huff
+"payable" @storage.type.interface.huff
+"nonpayable" @storage.type.interface.huff
+"external" @storage.type.interface.huff
+"internal" @storage.type.interface.huff
+"public" @storage.type.interface.huff
+"private" @storage.type.interface.huff
+"memory" @keyword.modifier.huff
+"storage" @keyword.modifier.huff
+"calldata" @keyword.modifier.huff
 
-(template_parameter_call) @type.parameter
+;; --------------------------
+;; Opcodes - Single category for now
+;; --------------------------
+(opcode) @entity.name.function.inputs.huff
+
+;; --------------------------
+;; Template Parameters
+;; --------------------------
+(template_parameter_call) @variable.parameter
 
 ;; --------------------------
 ;; Macro Calls
 ;; --------------------------
-(macro_call name: (identifier) @function)
-
-;; Jump opcodes (when parsed as macro calls)
-((macro_call name: (identifier) @keyword.control)
-  (#match? @keyword.control "^(jump|jumpi|jumpdest)$"))
-
+(macro_call name: (identifier) @entity.name.function.huff)
 
 ;; --------------------------
 ;; Constants
 ;; --------------------------
-(constant_definition name: (identifier) @constant)
-(constant_reference) @constant
+(constant_definition name: (identifier) @constant.name.huff)
+(constant_reference) @constant.name.huff
 
 ;; --------------------------
-;; Jump Targets (Labels & Jumptable Entries) - Blue Highlighting
+;; Numbers
 ;; --------------------------
-;; Jump label definitions (success:, error:, etc.)
-(jumpdest_label name: (identifier) @jumpdest_declaration)
-
-;; Jumptable entries (jump_one, jump_two, etc.)
-(jumptable_body (identifier) @function)
-
-;; Simple approach: Let jump opcodes be highlighted as control keywords
-;; Jump targets (labels and jumptable entries) are consistently highlighted as @label above
+(number_decimal) @constant.numeric.decimal.huff
+(number_hex) @constant.numeric.hexadecimal.huff
 
 ;; --------------------------
-;; New Declarations
+;; Jump Targets/Labels
 ;; --------------------------
-(declaration_table name: (identifier) @constant)
-(declaration_test name: (identifier) @function)
-(declaration_jumptable name: (identifier) @constant)
-
-;; --------------------------
-;; Identifiers
-;; --------------------------
-(identifier) @variable
+(jumpdest_label name: (identifier) @entity.name.function.huff)
+(jumpdest name: (identifier) @entity.name.function.huff)
+(jumptable_body (identifier) @entity.name.function.huff)
 
 ;; --------------------------
 ;; Builtin Functions
 ;; --------------------------
-(builtin_function) @function.builtin
-"__ERROR" @function.builtin
-"__EVENT_HASH" @function.builtin
-"__FUNC_SIG" @function.builtin
-"__RIGHTPAD" @function.builtin
-"__codesize" @function.builtin
-"__tablesize" @function.builtin
-"__tablestart" @function.builtin
+(builtin_function) @support.function.builtin.huff
+"__ERROR" @support.function.builtin.huff
+"__EVENT_HASH" @support.function.builtin.huff
+"__FUNC_SIG" @support.function.builtin.huff
+"__RIGHTPAD" @support.function.builtin.huff
+"__codesize" @support.function.builtin.huff
+"__tablesize" @support.function.builtin.huff
+"__tablestart" @support.function.builtin.huff
 
 ;; --------------------------
-;; Control/Include
+;; Control/Import
 ;; --------------------------
-(control_include) @keyword.import
-(control_include path: (string_literal) @string.special)
-
-;; --------------------------
-;; Decorators
-;; --------------------------
-(decorator) @attribute
-(decorator_item name: (identifier) @attribute)
-(decorator_item args: (string_literal) @string)
-(decorator_item args: (number) @number)
-
-;; --------------------------
-;; String Literals
-;; --------------------------
-(string_literal) @string
+"#include" @keyword.control.import.huff
+(control_include path: (string_literal) @string.quoted.double.huff)
 
 ;; --------------------------
 ;; Parameter Modifiers
 ;; --------------------------
-(modifier_indexed) @keyword
+(modifier_indexed) @keyword.modifier.huff
 
 ;; --------------------------
-;; Punctuation
+;; String Literals
 ;; --------------------------
-"(" @punctuation.bracket
-")" @punctuation.bracket
-"{" @punctuation.bracket
-"}" @punctuation.bracket
-"[" @punctuation.bracket
-"]" @punctuation.bracket
-":" @punctuation.delimiter
-"," @punctuation.delimiter
-"#[" @punctuation.bracket
-"]" @punctuation.bracket
+(string_literal) @string.quoted.double.huff
 
+;; --------------------------
+;; Decorators
+;; --------------------------
+(decorator) @meta.annotation.huff
+(decorator_item name: (identifier) @meta.annotation.huff)
+(decorator_item args: (string_literal) @string.quoted.double.huff)
+(decorator_item args: (number) @constant.numeric.decimal.huff)
+
+;; --------------------------
+;; Operators and Punctuation
+;; --------------------------
+"=" @keyword.operator.assignment.huff
+":" @punctuation.separator.huff
+"," @punctuation.separator.huff
+"(" @punctuation.bracket.round.huff
+")" @punctuation.bracket.round.huff
+"{" @punctuation.bracket.curly.huff
+"}" @punctuation.bracket.curly.huff
+"[" @punctuation.bracket.square.huff
+"]" @punctuation.bracket.square.huff
+"#[" @punctuation.bracket.square.huff
+
+;; --------------------------
+;; Generic Identifiers (fallback)
+;; --------------------------
+(identifier) @variable.other.readwrite.huff
